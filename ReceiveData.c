1fd67c3823370630adf80bb8fc1ec97a57db3db1
De-Li <34808088+De-Li@users.noreply.github.com>
2021-11-16 15:21:41 +0800

    Update ReceiveData.c

--- ReceiveData.c
+++ ReceiveData.c
@@ -42,10 +42,10 @@ int main(int argc , char *argv[]){
 	puts("Reply received\n");
 	puts(server_reply);
 	read(socket_desc, server_reply , 100);
-	/*for(int i =0;i<sizeof(server_reply);i++)
+	for(int i =0;i<sizeof(server_reply);i++)
 	{
-		printf(server_reply[i]);
-	}*/
+		printf("value of sensors: %s\n",server_reply[i]);
+	}
 	close(socket_desc);
 	sleep(2);
 	}
