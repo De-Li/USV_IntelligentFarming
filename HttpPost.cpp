ee55537e160e5ce0d75ce3ccf0f7e5ed2af1b740
De-Li <34808088+De-Li@users.noreply.github.com>
2021-11-24 12:21:21 +0800

    Create HttpPost.cpp

---
+++ HttpPost.cpp
@@ -0,0 +1,35 @@
+/*
+*****************************************************************
+Project: Unmmaned surface vehicle
+File name:HttpPost.cpp
+related file:HttpPost.h, GetWaterInfo.cpp
+function:Posting under water sensors to http server.
+author:De-Li
+version:1.0
+---------------------------------------------------------------
+Comment:
+---------------------------------------------------------------
+Log:
+---------------------------------------------------------------
+******************************************************************
+#include "HttpPost.h"
+#include "HTTPRequest.hpp"
+
+//constructor
+HttpPost::HttpPost(const string ip, const int port):m_Ip(ip), m_Port(port)
+{
+}
+
+//destructor
+HttpPost::~HttpPost()
+{
+}
+
+HttpPost::PostData()
+{
+  try
+  {
+    
+  }
+
+}
