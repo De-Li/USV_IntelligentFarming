3fb150757e13a06e702acf85bf4a5782558dc70c
De-Li <34808088+De-Li@users.noreply.github.com>
2021-11-29 16:10:27 +0800

    Update PostRainAndWater.py

--- PythonSocket/PostRainAndWater.py
+++ PythonSocket/PostRainAndWater.py
@@ -4,14 +4,17 @@ import serial, time
 
 if __name__ == '__main__':
     while(1):
-        CurrentRainData = int(GetRainData() or 0)
+        while(1):
+            CurrentRainData = int(GetRainData() or 0)
+            if(CurrentRainData != 0):
+                break
         HOST = '192.168.1.228'
         PORT = 30000
         #ClientMessage = 'Hello!'
     
         client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         #client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        print(CurrentRainData)
+        
         client.sendto(CurrentRainData.encode('utf-8'), (HOST, PORT))      
         serverMessage = str(client.recv(1024), encoding = 'utf-8')
         print('Server:', serverMessage)
