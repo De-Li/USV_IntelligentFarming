8bc3aa4da38258761531673f8f70558787172f11
De-Li <34808088+De-Li@users.noreply.github.com>
2021-11-29 16:13:33 +0800

    Update PostRainAndWater.py

--- PythonSocket/PostRainAndWater.py
+++ PythonSocket/PostRainAndWater.py
@@ -5,7 +5,7 @@ import serial, time
 if __name__ == '__main__':
     while(1):
         while(1):
-            CurrentRainData = GetRainData()
+            CurrentRainData = str(GetRainData() or 0)
             if(CurrentRainData != 0):
                 break
         HOST = '192.168.1.228'
