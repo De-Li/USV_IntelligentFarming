eea07c915c25d4d8923dffecc6314f951458f969
De-Li <34808088+De-Li@users.noreply.github.com>
2021-11-29 13:44:15 +0800

    Update ServerForRainAndWaterSensors.py.py

--- TestServer/ServerForRainAndWaterSensors.py.py
+++ TestServer/ServerForRainAndWaterSensors.py.py
@@ -1,17 +1,15 @@
 import socket
-HOST = ''
+
+HOST = '192.168.1.228'
 PORT = 30000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(10)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+print('create socket')
+s.bind((HOST, PORT))
+print('Listening for broadcast at ', s.getsockname())
 
 while True:
-    conn, addr = server.accept()
-    clientMessage = str(conn.recv(1024), encoding='utf-8')
-
-    print('Client message is:', clientMessage)
-
-    serverMessage = 'I\'m here!'
-    conn.sendall(serverMessage.encode())
-    conn.close()
+    data, address = s.recvfrom(65535)
+    print('Server received from {}:{}'.format(address, data.decode('utf-8')))
+    s.sendto("Hello it is server".encode('utf-8'),address)
