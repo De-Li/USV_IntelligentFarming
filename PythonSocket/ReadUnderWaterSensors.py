7a5b4ec48f3c920eec6f750c6e09519b45d85bbc
De-Li <34808088+De-Li@users.noreply.github.com>
2021-11-30 11:39:41 +0800

    Update ReadUnderWaterSensors.py

--- PythonSocket/ReadUnderWaterSensors.py
+++ PythonSocket/ReadUnderWaterSensors.py
@@ -48,9 +48,9 @@ def GetWaterData():
 
   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client.connect((HOST, PORT))
-  client.sendall(InquiryArray.encode())
+  client.sendall(InquiryArray)
 
-  serverMessage = str(client.recv(1024), encoding='utf-8')
+  serverMessage = str(client.recv(1024))
   print('Server:', serverMessage)
 
   client.close()
