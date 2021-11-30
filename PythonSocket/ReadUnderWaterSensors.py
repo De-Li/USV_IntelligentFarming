cbb5187ea13dc6911c1b8c8d6f3970c688d77d0e
De-Li <34808088+De-Li@users.noreply.github.com>
2021-11-30 13:24:32 +0800

    Update ReadUnderWaterSensors.py

--- PythonSocket/ReadUnderWaterSensors.py
+++ PythonSocket/ReadUnderWaterSensors.py
@@ -46,7 +46,7 @@ def GetWaterData():
   	#InquiryArray = bytearray()
   	#InquiryArray.append(0x01)
   	InquiryArray = bytes([0x01, 0x03, 0x00, 0x2b, 0x00, 0x01, 0xf4, 0x02])
-	print('Inquiry code : ')
+	print("Inquiry code : ")
 	print(InquiryArray)
   	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   	client.connect((HOST, PORT))
