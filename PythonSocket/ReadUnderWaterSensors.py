29953df4216dd22ec34d78a46d1c045ebf4673c5
De-Li <34808088+De-Li@users.noreply.github.com>
2021-11-30 13:26:42 +0800

    Update ReadUnderWaterSensors.py

--- PythonSocket/ReadUnderWaterSensors.py
+++ PythonSocket/ReadUnderWaterSensors.py
@@ -40,20 +40,19 @@ Create script for read under water sensors.
 import socket
 import time
 def GetWaterData():
-
   	HOST = '192.168.0.200'
   	PORT = 6969
-  	#InquiryArray = bytearray()
-  	#InquiryArray.append(0x01)
-  	InquiryArray = bytes([0x01, 0x03, 0x00, 0x2b, 0x00, 0x01, 0xf4, 0x02])
+	#InquiryArray = bytearray()
+	#InquiryArray.append(0x01)
+	InquiryArray = bytes([0x01, 0x03, 0x00, 0x2b, 0x00, 0x01, 0xf4, 0x02])
 	print("Inquiry code :")
 	print(InquiryArray)
-  	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  	client.connect((HOST, PORT))
-  	client.sendall(InquiryArray)
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.connect((HOST, PORT))
+	client.sendall(InquiryArray)
 
-  	serverMessage = client.recv(1024)
-  	print('Server:', serverMessage)
+	serverMessage = client.recv(1024)
+	print('Server:', serverMessage)
 
   	client.close()
 if __name__ == '__main__':
