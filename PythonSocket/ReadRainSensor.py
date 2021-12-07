11a4722afbd54c0624077f364dbc35bdbcc0f132
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-07 14:56:34 +0800

    Update ReadRainSensor.py

--- PythonSocket/ReadRainSensor.py
+++ PythonSocket/ReadRainSensor.py
@@ -45,10 +45,10 @@ while(flag):
 				print("Catch time:"+ str(datetime.now(tz).isoformat(timespec="seconds")))
 				'''
 				print(str(response))
-				return response
-				ser.close()
-				time.sleep(0.5)
-				flag = 0
+			return response
+			ser.close()
+			time.sleep(0.5)
+			flag = 0
 		except Exception as e1:
 			print ("communicating error " + str(e1))
 			return "NO return!" 
