94af7c9b75556c3fdfc131234dde3b1884dc9222
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-07 12:15:04 +0800

    Update PostWaterData.py

--- PythonSocket/PostWaterData.py
+++ PythonSocket/PostWaterData.py
@@ -8,14 +8,17 @@ author:De-Li
 version:1.0
 ---------------------------------------------------------------
 Comment:
+The Ip and port of Server
+	HOST = '140.116.202.132'
+	PORT = 3038
 2021/12/02
 The Ip and port of 5936 敬智's computer.
-    HOST = '192.168.0.110'
-    PORT = 6969
+	HOST = '192.168.0.110'
+	PORT = 6969
 2021/11/30
 The Ip and port of 5910 DeLi's computer.
-    HOST = '192.168.0.228'
-    PORT = 30000
+	HOST = '192.168.0.228'
+	PORT = 30000
     
 ---------------------------------------------------------------
 Log:
