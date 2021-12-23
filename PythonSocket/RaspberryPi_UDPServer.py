1d942b9d0ff74711d2db45283f71f6225d85ecb8
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-23 16:01:48 +0800

    Update RaspberryPi_UDPServer.py

--- PythonSocket/RaspberryPi_UDPServer.py
+++ PythonSocket/RaspberryPi_UDPServer.py
@@ -23,9 +23,9 @@ def GetWeatherDataFromESP8266():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
 	sock.bind((UDP_IP, UDP_PORT))
  
-while True:
-	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-	print("received message: %s" % data)
+	while True:
+		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+		print("received message: %s" % data)
 	
 if __name__ == '__main__':
 	while(1):
