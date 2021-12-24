73b29d9bebf70c43bb8e7cb9493bbe3a14400876
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-24 14:32:40 +0800

    Update RaspberryPi_UDPServer.py

--- PythonSocket/RaspberryPi_UDPServer.py
+++ PythonSocket/RaspberryPi_UDPServer.py
@@ -22,18 +22,18 @@ def GetWeatherDataFromESP8266():
 	Client_UDP_IP = "192.168.1.145"
 	Client_UDP_PORT = 5555
 
-	Receive_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
-	Receive_Sock.bind((Server_UDP_IP, Server_UDP_PORT))
+	#Receive_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
+	#Receive_Sock.bind((Server_UDP_IP, Server_UDP_PORT))
 	MESSAGE = "Hello this is server!"
 	#data, addr = Receive_Sock.recvfrom(1024) # buffer size is 1024 bytes
-	if(Receive_Sock.recvfrom(1024)):
-		data= Receive_Sock.recvfrom(1024) # buffer size is 1024 bytes
-		print("received message: %s")
-		print(data)
+	#if(Receive_Sock.recvfrom(1024)):
+		#data= Receive_Sock.recvfrom(1024) # buffer size is 1024 bytes
+		#print("received message: %s")
+		#print(data)
 	Send_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
 	Send_Sock.sendto(MESSAGE.encode('utf-8'), (Client_UDP_IP, Client_UDP_PORT))
 	Send_Sock.close()
-	Receive_Sock.close()
+	#Receive_Sock.close()
 	
 if __name__ == '__main__':
 	while(1):
