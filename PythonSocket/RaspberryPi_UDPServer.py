07e52e7aa304d1d71410710cf7c15f0e19ec99a8
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-27 15:38:59 +0800

    Update RaspberryPi_UDPServer.py

--- PythonSocket/RaspberryPi_UDPServer.py
+++ PythonSocket/RaspberryPi_UDPServer.py
@@ -44,7 +44,8 @@ def SendingMessageToESP8266():
 	Client_UDP_PORT = 5555
 	MESSAGE = "Hello this is raspberry pi!"
 	Send_Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Internet, # UDP
-	Send_Sock.sendto(MESSAGE.encode('utf-8'), (Client_UDP_IP, Client_UDP_PORT))
+	Send_Sock.send("Hello!")
+	#Send_Sock.sendto(MESSAGE.encode('utf-8'), (Client_UDP_IP, Client_UDP_PORT))
 	print("The message is Sent")
 	Send_Sock.close()
 if __name__ == '__main__':
