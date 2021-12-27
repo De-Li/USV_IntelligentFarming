0d212fa1a45449d5084a48de67a97db6ddf2058f
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-27 14:52:27 +0800

    Update RaspberryPi_UDPServer.py

--- PythonSocket/RaspberryPi_UDPServer.py
+++ PythonSocket/RaspberryPi_UDPServer.py
@@ -21,13 +21,11 @@ Log:
 ---------------------------------------------------------------
 """
 import socket, time
-
+Server_UDP_IP = "192.168.1.56"
+Server_UDP_PORT = 7777
+Client_UDP_IP = "192.168.1.145"
+Client_UDP_PORT = 5555
 def GetWeatherDataFromESP8266():
-	Server_UDP_IP = "192.168.1.56"
-	Server_UDP_PORT = 7777
-	Client_UDP_IP = "192.168.1.145"
-	Client_UDP_PORT = 5555
-
 	Receive_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
 	Receive_Sock.bind((Server_UDP_IP, Server_UDP_PORT))
 	data, addr = Receive_Sock.recvfrom(1024) # buffer size is 1024 bytes
@@ -42,6 +40,11 @@ def GetWeatherDataFromESP8266():
 	'''
 	Receive_Sock.close()
 	return data.decode("utf-8")
+def SendingMessageToESP8266():
+	MESSAGE = "Hello this is raspberry pi!"
+	Send_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
+	Send_Sock.sendto(MESSAGE.encode('utf-8'), (Client_UDP_IP, Client_UDP_PORT))
+	Send_Sock.close()
 if __name__ == '__main__':
 	while(1):
 		GetWeatherDataFromESP8266()
