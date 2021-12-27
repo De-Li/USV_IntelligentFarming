ad5f2ef3a09560f43e32c9c9b197d77ec986a137
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-27 14:54:36 +0800

    Update RaspberryPi_UDPServer.py

--- PythonSocket/RaspberryPi_UDPServer.py
+++ PythonSocket/RaspberryPi_UDPServer.py
@@ -21,11 +21,10 @@ Log:
 ---------------------------------------------------------------
 """
 import socket, time
-Server_UDP_IP = "192.168.1.56"
-Server_UDP_PORT = 7777
-Client_UDP_IP = "192.168.1.145"
-Client_UDP_PORT = 5555
+
 def GetWeatherDataFromESP8266():
+	Server_UDP_IP = "192.168.1.56"
+	Server_UDP_PORT = 7777
 	Receive_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
 	Receive_Sock.bind((Server_UDP_IP, Server_UDP_PORT))
 	data, addr = Receive_Sock.recvfrom(1024) # buffer size is 1024 bytes
@@ -41,13 +40,16 @@ def GetWeatherDataFromESP8266():
 	Receive_Sock.close()
 	return data.decode("utf-8")
 def SendingMessageToESP8266():
+	Client_UDP_IP = "192.168.1.145"
+	Client_UDP_PORT = 5555
 	MESSAGE = "Hello this is raspberry pi!"
 	Send_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
 	Send_Sock.sendto(MESSAGE.encode('utf-8'), (Client_UDP_IP, Client_UDP_PORT))
 	Send_Sock.close()
 if __name__ == '__main__':
 	while(1):
-		GetWeatherDataFromESP8266()
+		#GetWeatherDataFromESP8266()
+		SendingMessageToESP8266()
 		time.sleep(1)
 	
 	
