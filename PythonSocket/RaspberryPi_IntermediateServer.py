391f6960091d581826500607378d8b97c416deac
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-29 14:43:04 +0800

    Update and rename RaspberryPi_UDPServer.py to RaspberryPi_IntermediateServer.py

--- PythonSocket/RaspberryPi_IntermediateServer.py
+++ PythonSocket/RaspberryPi_IntermediateServer.py
@@ -1,7 +1,7 @@
 """
 *****************************************************************
 Project: Unmmaned surface vehicle
-File name:RaspberryPi_UDPServer.py
+File name:RaspberryPi_IntermediateServer.py
 related file:
 function:Get weather data from ESP8266 client to RaspberryPi UDP server.
 author:De-Li
@@ -10,19 +10,20 @@ version:1.0
 Comment:
 	In 5910 Lab
 	RaspberryPi
-	Server_UDP_IP = "192.168.1.56" 
-	Server_UDP_PORT = 7777
-	Arduino
-	Client_UDP_IP = "192.168.1.145"
-	Client_UDP_PORT = 5555
+	Server IP = "192.168.1.56" 
+	Serve PORT = 7777
+	ESP8266
+	Client IP = "192.168.1.145"
+	Client PORT = 5555
 ---------------------------------------------------------------
 Log:
-
+add SendingMessageToFloatChamber() function to control the electrical power on the float chamber.
 ---------------------------------------------------------------
 """
 import socket, time
 
-def GetWeatherDataFromESP8266():
+def GetWeatherDataFromGroundStation():
+	#Get Weather Data From ESP8266 on the ground station
 	Server_UDP_IP = "192.168.1.56"
 	Server_UDP_PORT = 7777
 	Receive_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
@@ -31,20 +32,19 @@ def GetWeatherDataFromESP8266():
 	if(Receive_Sock.recvfrom(1024)):
 		print("received message: ")
 		print(data)
-	'''
-	#No need to talk to the client.
-	Send_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
-	Send_Sock.sendto(MESSAGE.encode('utf-8'), (Client_UDP_IP, Client_UDP_PORT))
-	Send_Sock.close()
-	'''
+
 	Receive_Sock.close()
 	return data.decode("utf-8")
-def SendingMessageToESP8266(command):
+
+def SendingMessageToFloatChamber(command):
+	#Send Message To ESP8266 on the Float chamber
 	Client_TCP_IP = "192.168.1.145"
 	Client_TCP_PORT = 5555
-	MESSAGE = "Hello this is raspberry pi!"
-	Send_Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Internet, # UDP
+	#MESSAGE = "Hello this is raspberry pi!"
+	#Create TCP socket
+	Send_Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Internet, # TCP
 	Send_Sock.connect((Client_TCP_IP,Client_TCP_PORT))
+	#Check the command
 	if command==ShowVoltage:
 		Send_Sock.send(command.encode('utf-8'))
 	elif command==ShutDown:
@@ -53,12 +53,13 @@ def SendingMessageToESP8266(command):
 		Send_Sock.send(command.encode('utf-8'))
 	print(Send_Sock.recv(200))
 	return Send_Sock.recv(200)
+	#close the socket
 	Send_Sock.close()
 	
 if __name__ == '__main__':
 	while(1):
-		#GetWeatherDataFromESP8266()
-		SendingMessageToESP8266()
+		#GetWeatherDataFromGroundStation()
+		SendingMessageToFloatChamber()
 		time.sleep(1)
 	
 	
