ba4c72ebae7a4c21551c48bac070c5c6f1b47cd5
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-29 15:42:36 +0800

    Update ReadRainSensor.py

--- PythonSocket/ReadRainSensor.py
+++ PythonSocket/ReadRainSensor.py
@@ -1,6 +1,11 @@
-import serial, time
-from datetime import datetime, timezone, timedelta
-
+import serial, time, re
+#from datetime import datetime, timezone, timedelta
+def DecipherRainData(response):
+	#extract the float data from response
+	FloatData = re.findall("\d+\.\d+", response)
+	#arrange rain data into CSV format. 
+	ArrangedResponse = '['+', '.join(str(e) for e in FloatData)+']'
+	return ArrangedResponse
 def GetRainData():
   '''
   #  +8 timezone
@@ -24,31 +29,32 @@ def GetRainData():
 	ser.rtscts = False     #disable hardware (RTS/CTS) flow control
 	ser.dsrdtr = False     #disable hardware (DSR/DTR) flow control
 	flag = 1;
-	while(flag):
-		try: 
-			ser.open()        
-		except Exception as ex:
-			print ("open serial port error " + str(ex))
-			exit() 
-		if ser.isOpen():
+	try: 
+		ser.open()        
+	except Exception as ex:
+		print ("open serial port error " + str(ex))
+		exit() 
+	if ser.isOpen():
+		while(1):
 			try:
 				ser.flushInput() #flush input buffer
 				ser.flushOutput() #flush output buffer  
-     
+				
 				#listen data per 0.5s
 				response = ser.readline()
 				if(response):
-					print("read data:")
-					'''
-					response = str(response) + "Catch time:"+ str(datetime.now(tz).isoformat(timespec="seconds"))
+				print("read data:")
+				'''
+				response = str(response) + "Catch time:"+ str(datetime.now(tz).isoformat(timespec="seconds"))
+				print(str(response))
+				print("Catch time:"+ str(datetime.now(tz).isoformat(timespec="seconds")))
+				'''
 					print(str(response))
-					print("Catch time:"+ str(datetime.now(tz).isoformat(timespec="seconds")))
-					'''
-					print(str(response))
-				return response
-				ser.close()
+					ser.close()
+					#The sequence of response is Acc(Accumulation), EventAcc, TotalAcc, RInt.
+					ArrangedResponse = DecipherRainData(response)
+					return ArrangedResponse
 				time.sleep(0.5)
-				flag = 0
 			except Exception as e1:
 				print ("communicating error " + str(e1))
 				return "NO return!" 
@@ -58,3 +64,4 @@ def GetRainData():
 
 if __name__ == '__main__':
 	RainData = GetRainData()
+	time.sleep(1)
