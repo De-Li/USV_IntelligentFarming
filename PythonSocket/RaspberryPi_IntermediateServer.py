5a096746c5f92816b9d5881cba6598c6e042a590
De-Li <34808088+De-Li@users.noreply.github.com>
2021-12-29 15:14:56 +0800

    Update RaspberryPi_IntermediateServer.py

--- PythonSocket/RaspberryPi_IntermediateServer.py
+++ PythonSocket/RaspberryPi_IntermediateServer.py
@@ -3,7 +3,7 @@
 Project: Unmmaned surface vehicle
 File name:RaspberryPi_IntermediateServer.py
 related file:
-function:Get weather data from ESP8266 client to RaspberryPi UDP server.
+function:Let the raspberry pi become the intermediate role to convey the message between main server and endpoint.
 author:De-Li
 version:1.0
 ---------------------------------------------------------------
@@ -17,27 +17,44 @@ Comment:
 	Client PORT = 5555
 ---------------------------------------------------------------
 Log:
+2021/12/29
 add SendingMessageToFloatChamber() function to control the electrical power on the float chamber.
+add GetCommandFromMainServer() function to receive the command from the main server.
 ---------------------------------------------------------------
 """
 import socket, time
 
 def GetWeatherDataFromGroundStation():
-	#Get Weather Data From ESP8266 on the ground station
+	#Get weather data from ESP8266 on the ground station
 	Server_UDP_IP = "192.168.1.56"
-	Server_UDP_PORT = 7777
+	Server_UDP_PORT_ForESP8266 = 7777
 	Receive_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
-	Receive_Sock.bind((Server_UDP_IP, Server_UDP_PORT))
-	data, addr = Receive_Sock.recvfrom(1024) # buffer size is 1024 bytes
-	if(Receive_Sock.recvfrom(1024)):
+	Receive_Sock.bind((Server_UDP_IP, Server_UDP_PORT_ForESP8266))
+	data, addr = Receive_Sock.recvfrom(100) # buffer size is 100 bytes
+	if(Receive_Sock.recvfrom(100)):
 		print("received message: ")
 		print(data)
 
 	Receive_Sock.close()
 	return data.decode("utf-8")
 
+def GetCommandFromMainServer():
+	#Get command from main server(Website)
+	Server_UDP_IP = "192.168.1.56"
+	Server_UDP_PORT_ForMainServer = 6666
+	Receive_Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, # UDP
+	Receive_Sock.bind((Server_UDP_IP, Server_UDP_PORT_ForMainServer))
+	command, addr = Receive_Sock.recvfrom(100) # buffer size is 1024 bytes
+	if(Receive_Sock.recvfrom(100)):
+		print("received message: ")
+		print(command)
+		SendingMessageToFloatChamber(command)
+
+	Receive_Sock.close()
+	return data.decode("utf-8")
+
 def SendingMessageToFloatChamber(command):
-	#Send Message To ESP8266 on the Float chamber
+	#Raspberry Pi send message to ESP8266 on the Float chamber
 	Client_TCP_IP = "192.168.1.145"
 	Client_TCP_PORT = 5555
 	#MESSAGE = "Hello this is raspberry pi!"
