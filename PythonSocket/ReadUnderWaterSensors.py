0d0cdcf88b52800d720ad5cbaf12092003009d7e
De-Li <34808088+De-Li@users.noreply.github.com>
2022-01-19 12:23:46 +0800

    Update ReadUnderWaterSensors.py

--- PythonSocket/ReadUnderWaterSensors.py
+++ PythonSocket/ReadUnderWaterSensors.py
@@ -58,6 +58,7 @@ def GetWaterData():
 	InquiryArray_AmmoniaNitrogen = bytes([0x01, 0x03, 0x00, 0x01, 0x00, 0x02, 0x95, 0xcb])
 	
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.settimeout(3)
 	try:
 		client.connect((HOST, PORT))
 		client.settimeout(1)
