40cb5b0de1c8bd127d41b1ec91ca0f1cad752725
De-Li <34808088+De-Li@users.noreply.github.com>
2022-01-27 17:36:45 +0800

    Update Main_PostRainAndWater.py

--- PythonSocket/Main_PostRainAndWater.py
+++ PythonSocket/Main_PostRainAndWater.py
@@ -73,9 +73,9 @@ RainData = ", 0, 0, 0, 0, 0]"
 #VoltageLimit = 10.8
 #Time(second)
 UploadInterval = 600
-WaterSampleInterval = 20 #UploadInterval*(5/6)
+WaterSampleInterval = UploadInterval*(5/6)
 WaterPowercontrolTryingLimit = 10
-SampleInterval = UploadInterval/3 #UploadInterval/3
+SampleInterval = 20 #UploadInterval/3
 MinTransmitTimeInterval = 5
 SocketTimeOut = 1
 WaitingLimit = 20
