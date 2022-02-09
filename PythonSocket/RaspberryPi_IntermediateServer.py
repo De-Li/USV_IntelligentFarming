499f3cb605cf9115e28770ac2c3ffc6128762d61
De-Li <34808088+De-Li@users.noreply.github.com>
2022-02-09 14:04:48 +0800

    Update RaspberryPi_IntermediateServer.py

--- PythonSocket/RaspberryPi_IntermediateServer.py
+++ PythonSocket/RaspberryPi_IntermediateServer.py
@@ -96,9 +96,9 @@ def SendingMessageToFloatChamber(command):
 	#MESSAGE = "Hello this is raspberry pi!"
 	#Create TCP socket
 	Send_Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Internet, # TCP
-	Send_Sock.settimeout(10)
+	Send_Sock.settimeout(1)
 	try:
-		Send_Sock.connect((Client_TCP_IP,Client_TCP_PORT))
+		Send_Sock.connect((Client_TCP_IP, Client_TCP_PORT))
 	except:
 		ReturnList.append("[None, None")
 		ReturnList.append("Lose connection to the ESP8266 on the Float chamber")
