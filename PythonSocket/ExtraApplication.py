89c427a9da406b221ff4bbc59772253e7470c7b8
De-Li <34808088+De-Li@users.noreply.github.com>
2022-03-14 14:41:03 +0800

    Update ExtraApplication.py

--- PythonSocket/ExtraApplication.py
+++ PythonSocket/ExtraApplication.py
@@ -1,8 +1,15 @@
 # importing geopy library
 from geopy.geocoders import Nominatim
 import os 
+def GetGPSCoordination():
+	lat,lon = os.popen('curl ipinfo.io/loc').read().split(',')
 
-lat,lon = os.popen('curl ipinfo.io/loc').read().split(',')
-
-print("Latitude = ", lat, "\n")
-print("Longitude = ", lon)
+	print("Latitude = ", lat, "\n")
+	print("Longitude = ", lon)
+	return lat, lon
+if __name__ == '__main__':
+	while(1):  
+		#get water information
+		GetGPSCoordination()
+		#Sleep 1 second
+		time.sleep(2)
