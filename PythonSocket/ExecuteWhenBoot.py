c41c385db5f9026fe79cd36e3722eff0b1bff7f2
De-Li <34808088+De-Li@users.noreply.github.com>
2022-03-21 10:49:25 +0800

    Update ExecuteWhenBoot.py

--- PythonSocket/ExecuteWhenBoot.py
+++ PythonSocket/ExecuteWhenBoot.py
@@ -5,8 +5,11 @@ time.sleep(30)
 while True:
 	print("stream:")
 	print(stream)
+	'''
 	if(stream is not "<os._wrap_close object at 0xb6662750>"):
 		stream = os.popen('ffmpeg -r 30 -video_size 640x480 -loglevel +repeat -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -c:a aac -ar 44100 -f flv -r 30 rtmp://140.116.202.132:3033/live/UnderWaterVideoKaohsiung')
+	time.sleep(60)
+	'''
 	time.sleep(60)
 #print("There is something wrong, wait for solving")
 #time.sleep(10)
