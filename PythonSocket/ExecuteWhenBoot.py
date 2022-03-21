0fc9b951a2e6bd23ea61f86c541ba5d4a0da6a6d
De-Li <34808088+De-Li@users.noreply.github.com>
2022-03-21 10:10:30 +0800

    Update ExecuteWhenBoot.py

--- PythonSocket/ExecuteWhenBoot.py
+++ PythonSocket/ExecuteWhenBoot.py
@@ -1,6 +1,13 @@
 import os, time
-#while True:
-stream = os.popen('ffmpeg -r 10 -re -i http://192.168.1.129/webcam/?action=stream -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv -r 10 rtmp://140.116.202.132:3033/live/UnderWaterVideoKaohsiung')
+stream = 0
+stream = os.popen('ffmpeg -r 30 -video_size 640x480 -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -c:a aac -ar 44100 -f flv -r 30 rtmp://140.116.202.132:3033/live/UnderWaterVideoKaohsiung')
+while True:
+  print("stream:")
+  print(stream)
+  if(stream is not 0)
+  {
+    stream = os.popen('ffmpeg -r 30 -video_size 640x480 -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -c:a aac -ar 44100 -f flv -r 30 rtmp://140.116.202.132:3033/live/UnderWaterVideoKaohsiung')
+  }
   #print("There is something wrong, wait for solving")
   #time.sleep(10)
   #stream = os.popen('sudo service webcamd restart')
