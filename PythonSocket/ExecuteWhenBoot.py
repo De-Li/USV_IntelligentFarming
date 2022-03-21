a1459b51ca73eaa1bc7c50a77423311cafc54c9c
De-Li <34808088+De-Li@users.noreply.github.com>
2022-03-21 11:13:45 +0800

    Update ExecuteWhenBoot.py

--- PythonSocket/ExecuteWhenBoot.py
+++ PythonSocket/ExecuteWhenBoot.py
@@ -1,12 +1,14 @@
 import os, time
+from processing import Process
 stream = 0
-stream = os.popen('ffmpeg -r 30 -video_size 640x480 -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -c:a aac -ar 44100 -f flv -r 30 rtmp://140.116.202.132:3033/live/UnderWaterVideoKaohsiung -loglevel debug 2> wildlife.txt')
-time.sleep(30)
+stream = os.popen('ffmpeg -r 30 -video_size 640x480 -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -c:a aac -ar 44100 -f flv -r 30 rtmp://140.116.202.132:3033/live/UnderWaterVideoKaohsiung')
+#time.sleep(30)
 while True:
 	print("stream:")
 	print(stream)
-	time.sleep(60)
+	time.sleep(20)
 	process.StandardInput.WriteLine("q");
+	print("kill the ffmpeg")
 	time.sleep(10)
 	stream = os.popen('ffmpeg -r 30 -video_size 640x480 -loglevel +repeat -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -c:a aac -ar 44100 -f flv -r 30 rtmp://140.116.202.132:3033/live/UnderWaterVideoKaohsiung')
 	
